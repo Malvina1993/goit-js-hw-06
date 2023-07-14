3d3791095d5335db4a150cdafaf61d63d839d6a6
Malvina1993 <mykhamalvina@gmail.com>
2023-07-14 22:52:49 +0300

    added example 2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.getElementById('ingredients');
+
+const liEl = ingredients
+  .map(ingr => ingr = `<li class="item">${ingr}</li>`)
+  .join("");
+
+
+
+ulEl.insertAdjacentHTML('afterbegin', liEl);
+
+
