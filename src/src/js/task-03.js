2790d5ae2b9e1cc9131da5486bb3de43fa43705e
Malvina1993 <mykhamalvina@gmail.com>
2023-07-14 23:31:40 +0300

    have done 3 example

--- src/src/js/task-03.js
+++ src/src/js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const ulEl = document.querySelector('ul.gallery');
+
+const liEls = images
+  .map(({ url, alt }) => `<li><img src="${url}" alt="${alt}"></li>`)
+  .join('')
+
+
+
+ulEl.insertAdjacentHTML("afterbegin", liEls);
+
