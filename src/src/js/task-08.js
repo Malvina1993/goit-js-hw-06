ed72adab9e24bc9abb16ce79f48057e0d41133eb
Malvina1993 <mykhamalvina@gmail.com>
2023-07-17 17:22:15 +0300

    added 8 example

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,27 @@
 
+const formEl = document.querySelector('.login-form');
+const inpEmail = document.querySelector('[type="email"]');
+const inpPassword = document.querySelector('[type="password"]');
+// const btnForm = document.querySelector('[type="submit"]');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    if ((event.currentTarget.elements.email.value === "")||(event.currentTarget.elements.password.value === "")) {
+         alert('All fields must be filled');
+    } else {
+        const formDate = {
+            email: `${event.currentTarget.elements.email.value}`,
+            password: `${event.currentTarget.elements.password.value}`,
+        };
+        console.log(formDate);
+        formEl.reset();
+
+    }
+
+
+};
+
+
